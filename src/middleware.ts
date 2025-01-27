55c2cdeeb44a8f562e8781c9dfd5c951129c2b33
bappi12312 <ranamdbappihossain@gmail.com>
2025-01-27 09:29:10 +0600

    apply middle ware to all routees

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const accessToken = req.cookies.get("accessToken")?.value;
+
+  const publicRoutes = ["/","/login", "/register"];
+  const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
+
+  if (accessToken && ["/login","/register"].includes(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!accessToken && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next|favicon.ico).*)"],
+};
